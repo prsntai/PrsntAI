6e00affd97681738ef121be92a23fa4a5faa2e35
Koral Kulacoglu <kulacoglukoral@gmail.com>
2024-01-05 22:23:57 -0500

    added animations

--- prsntai/src/pages/Home.js
+++ prsntai/src/pages/Home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Typewriter from 'typewriter-effect';
 import './styles/style.css';
 import './styles/background.css';
+import './styles/start.css';
 
 const Home = () => {
     return (
@@ -21,7 +22,7 @@ const Home = () => {
                 </ul>
             </div>
             <div className="body">
-                <div className="nav">
+                <div class="nav">
                     <a href="/"><img src="https://i.ibb.co/wJD0wwv/Prsnt-AILogo-No-BG.png" className="nav-logo" alt="logo"/></a>
                     <a href="/" className="nav-element">
                         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -31,9 +32,9 @@ const Home = () => {
                     <a href="/docs" className="nav-element">Docs</a>
                     <a href="https://github.com/prsntai" target="_blank" rel="noreferrer" className="nav-right">GitHub</a>
                 </div>
-                <img className="title-logo" src="https://i.ibb.co/rMpr4fS/prsnt-AILinked-In.jpg" alt="PrsntAI Logo"/>
-                <h1>PrsntAI</h1>
-                <div class="typewriter">
+                <img class="title-logo spin__animation" src="https://i.ibb.co/rMpr4fS/prsnt-AILinked-In.jpg" alt="PrsntAI Logo"/>
+                <h1 class="appear__animation">PrsntAI</h1>
+                <div class="typewriter appear__animation">
                     <Typewriter 
                         options={{
                         strings: ['Create real-time slideshows with your voice', "Powered by ChatGPT & DALL-E"],
@@ -43,7 +44,7 @@ const Home = () => {
                         deleteSpeed: 25,
                     }}/>
                 </div>
-                <a href="/ai" className="try-button">Try it out!</a>
+                <a href="/ai" className="try-button appear__animation">Try it out!</a>
             </div>
             <footer><p>&copy; 2023 PrsntAI</p></footer>
         </div>
